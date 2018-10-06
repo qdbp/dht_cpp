ea17e204497f049662179ef65b57b3264a4998f2
Evgeny Naumov <evgeny.a.naumov@gmail.com>
2018-10-06 00:33:05 -0400

    >> first, manual working version of eyes.py

--- cht/eyes.py
+++ cht/eyes.py
@@ -1,8 +1,41 @@
-import numpy as np
-import pandas as pd
+import matplotlib
 
-import maptlotlib.pyplot as plt
+# breaker
+
+matplotlib.use('cairo')
 
 
-class RunningPlot():
-    pass
+def main():
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+
+    plt.style.use('ggplot')
+
+    df = pd.read_csv('./data/stat.csv').iloc[:, :-1]
+    df.set_index('time', inplace=True)
+
+    del df['ctl_ping_thresh']
+
+    cols = df.columns.copy()
+
+    for col in cols:
+        if ('rx_q_' not in col) and ('tx_r_' not in col) \
+                and ('rx_r_' not in col) and ('tx_q_' not in col) \
+                or ('_ap' in col) or ('tx_r_' in col):
+
+            del df[col]
+
+    df = df.diff() + 1
+    df.plot()
+
+    plt.yscale('log')
+    plt.legend()
+    plt.gcf().set_size_inches((1920/150, 1080/150))
+    plt.gcf().set_dpi(150)
+    plt.tight_layout()
+    plt.savefig('plot.png')
+
+
+if __name__ == '__main__':
+    main()
